f539dd013926a9009a2f268e026c8023ef7d3ee1
unknown <niloofar.mojoodi@yahoo.com>
2019-10-19 19:57:45 +0330

    Updates

---
+++ precache-manifest.087ff69a5ba871fe250495d56a41349b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "244cde1267a71751b6eef50cef47bd8d",
+    "url": "/my_resume/index.html"
+  },
+  {
+    "revision": "5313c41e02a246728504",
+    "url": "/my_resume/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "df0549f5f93acd8c02b8",
+    "url": "/my_resume/static/css/main.2c2729ce.chunk.css"
+  },
+  {
+    "revision": "5313c41e02a246728504",
+    "url": "/my_resume/static/js/2.32fe11f2.chunk.js"
+  },
+  {
+    "revision": "df0549f5f93acd8c02b8",
+    "url": "/my_resume/static/js/main.e148a69c.chunk.js"
+  },
+  {
+    "revision": "6685d17bedb2843911d1",
+    "url": "/my_resume/static/js/runtime~main.07570d3b.js"
+  }
+]);
